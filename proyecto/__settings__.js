e7cc4681f4eb285162b723e23f73253289de646d
Mariru1z <maria.ruizsa@anahuac.mx>
2024-09-23 13:27:13 -0600

    Añadida la carpeta mi_carpeta al repositorio

---
+++ proyecto/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2076674.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 196911636, 196911637, 196911660, 196911640, 196911641, 196911642, 196911643, 196911644, 196911646, 196911647, 196911648, 196911649, 196911651, 196911652 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/196911657/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/196911658/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/196911656/1/ammo.js', 'preload' : true},
+];
